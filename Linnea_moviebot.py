89b9105bea88a71fdc8a5b04d7f35a366800483c
linneaskold <linnea.skold@elev.ga.lbs.se>
2026-04-08 11:03:47 +0200

    Minst 10 entries. Minst 4 egenskaper.

---
+++ Linnea_moviebot.py
@@ -0,0 +1,19 @@
+
+from turtle import title
+
+
+movie = [
+    {"title":"Iron Man","genre":"Action","mood":"Confident","age":"13+"},
+    {"title":"Howls Moving Castle","genre":"Anime","mood":"Magical","age":"¨7+"},
+    {"title":"Scream","genre":"Scary","mood":"Psychological","age":"16+"},
+    {"title":"A Silent Voice","genre":"anime","mood":"emotional","age":"13+"},
+    {"title":"I Want To Eat Your Pancreas","genre":"anime","mood":"bittersweet","age":"13+"},
+    {"title":"Jumanji","genre":"Adventure","mood":"Fun","age":"13+"},
+    {"title":"Alien","genre":"Sci-fi","mood":"isolation","age":"16+"},
+    {"title":"Resident Evil","genre":"Action","mood":"dread","age":"16+"},
+    {"title":"Miss Peregrines Home For Peculiar Children","genre":"Fantasy","mood":"dark","age":"13+"},
+    {"title":"The princess and the frog","genre":"Animation","mood":"nostalgic","age":"7+"},
+    {"title":"Guardians of the Galaxy","genre":"Action","mood":"fun","age":"13+"},
+    {"title":"Suzume","genre":"Anime","mood":"adventure","age":"13+"},
+    {"title":"The tunnel to summer, the exit of goodbyes","genre":"Anime","mood":"bittersweet","age":"13+"},
+    ]
